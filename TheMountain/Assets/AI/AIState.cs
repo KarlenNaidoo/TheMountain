d57dbf23065241b30a0b7bcf4104d1b96e7e77e9
KarlenNaidoo <karlen.naidoo@gmail.com>
2018-07-03 22:25:48 +1000

    Adding StateMachine behaviour scripts

---
+++ TheMountain/Assets/AI/AIState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIState : MonoBehaviour {
+
+    public abstract AIStateType GetStateType();
+    public abstract AIStateType OnUpdate();
+
+    public virtual void OnEnterState() { }
+    public virtual void OnExitState() { }
+    public virtual void OnAnimatorUpdated() { } // so Unity does not call OnAnimatorMove for all states that arent even active
+    public virtual void OnAnimatorIKUpdated() { }
+    public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) { }
+    public virtual void OnDesitinationReached(bool isReached) { }
+    // store a reference to its parent state
+    public void SetStateMachine(AIStateMachine statemachine) { statemachine = _statemachine; }
+
+    protected AIStateMachine _statemachine;
+}
---
+++ TheMountain/Assets/AI/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Every collider will be stored in a dictionary with a reference to its statemachine
+ */ 
+public class GameSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
