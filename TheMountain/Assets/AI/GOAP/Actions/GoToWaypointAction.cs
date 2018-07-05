09ab8dc96944f38f74b67acc22adc7085f05b943
KarlenNaidoo <karlen.naidoo@gmail.com>
2018-07-05 22:01:39 +1000

    Implementing the GOAP framwork.
    Currently working with one action

---
+++ TheMountain/Assets/AI/GOAP/Actions/GoToWaypointAction.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class GoToWaypointAction : GoapAction
+{
+    private bool _reached = false;
+    private Waypoint _targetWaypoint;
+
+    float _startTime = 0;
+    
+    public GoToWaypointAction()
+    {
+        AddPreConditions("visible", true); // waypoint must be visible
+        AddEffect("reachWayPoint", true); // we reached our destination
+    }
+
+    public override bool CheckProceduralPreCondtions(GameObject agent)
+    {
+        Debug.Log("Reached here");
+        //find the nearest waypoint
+        List<Waypoint> waypointNetwork = new List<Waypoint>();
+        foreach (Waypoint point in Resources.FindObjectsOfTypeAll<Waypoint>())
+        {
+            waypointNetwork.Add(point);
+        }
+        
+        Debug.Log("Number of waypoints is " + waypointNetwork.Count);
+        Waypoint _closest = null;
+        float _closestDist = 0;
+        foreach (Waypoint point in waypointNetwork)
+        {
+            if(_closest == null)
+            {
+                _closest = point;
+                _closestDist = (point.gameObject.transform.position - agent.transform.position).magnitude;
+            }
+            else
+            {
+                float _dist = (point.gameObject.transform.position - agent.transform.position).magnitude;
+                if(_dist < _closestDist)
+                {
+                    _closest = point;
+                    _closestDist = _dist;
+                }
+            }
+        }
+        if (_closest == null)
+            return false;
+        _targetWaypoint = _closest;
+        target = _closest.gameObject;
+        Debug.Log("Removing " + target);
+        waypointNetwork.Remove(_closest);
+        return _closest != null;
+    }
+
+    public override bool IsDone()
+    {
+        return _reached;
+    }
+
+    public override bool Perform(GameObject agent)
+    {
+        Debug.Log("Finished this action");
+        _reached = true;
+
+        return true;
+    }
+
+    public override bool RequiresInRange()
+    {
+        return true; // yes we need to be in range of the waypoint to have reached it
+    }
+
+    public override void Reset()
+    {
+        _reached = false;
+        _targetWaypoint = null;
+    }
+}
---
+++ TheMountain/Assets/AI/GOAP/Agents/AICharacter.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+/**
+ * A general AI class.
+ * Subclass this for specific Character classes and implement the createGoalState() method that will populate the goal for the GOAP planner.
+ */ 
+public abstract class AICharacter : MonoBehaviour, IGoap
+{
+    public float moveSpeed = 2f;
+    public bool isVisible = true;
+    NavMeshAgent _navAgent;
+    public void actionsFinished()
+    {
+        // everything is done, we completed our actions for this goal
+        Debug.Log("ACTIONS COMPLETED");
+    }
+
+    public abstract Dictionary<string, object> CreateGoalState();
+
+    // Dictionary that will feed the GOAP actions and system while planning
+    public Dictionary<string, object> GetWorldState()
+    {
+        Dictionary<string, object> worldData = new Dictionary<string, object>();
+        worldData.Add("visible", isVisible);
+        //worldData.Add("reachWayPoint", true);
+        return worldData;
+
+    }
+
+    public void Awake()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();  
+    }
+    public bool MoveAgent(GoapAction nextAction)
+    {
+        Debug.Log("Next Action " + nextAction);
+        Debug.Log("Next action target " + nextAction.target.transform.position);
+        // move towards the Next actions target
+        float step = moveSpeed * Time.deltaTime;
+        _navAgent.SetDestination(nextAction.target.transform.position);
+
+        if ((gameObject.transform.position - nextAction.target.transform.position).magnitude < 2f)
+        {
+            //we are at the target location, we are done
+            nextAction.inRange = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void PlanAborted(GoapAction aborter)
+    {
+        // An action bailed out of the plan. State has been reset to plan again.
+        // Take note of what happened and make sure if you run the same goal again
+        // that it can succeed.
+        Debug.Log("PLAN ABORTED");
+    }
+
+    public void PlanFailed(Dictionary<string, object> failedGoal)
+    {
+        // Not handling this here since we are making sure our goals will always succeed.
+        // But normally you want to make sure the world state has changed before running
+        // the same goal again, or else it will just fail.
+    }
+
+    public void PlanFound(Dictionary<string, object> goal, Queue<GoapAction> actions)
+    {
+        Debug.Log("PLAN FOUND");
+    }
+}
